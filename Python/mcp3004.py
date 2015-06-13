ca234057bfe5d9f1eaa893b972e947a9eda5bd1b
Cavon <cavonxx@gmail.com>
2015-06-13 01:57:05 +0000

    lots

---
+++ Python/mcp3004.py
@@ -0,0 +1,22 @@
+import spidev
+import time
+
+#Establish SPI connection with Bus 0, Device 0
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+def get_adc(channel):
+	#Perform SPI transaction and store returned bits in 'r'
+	r = spi.xfer([1, (8+channel)<<4, 0])
+	#Filter data bits from returned bits
+	adcout = ((r[1]&3) << 8) + r[2]
+	#Return value from 0-1023
+	return adcout
+
+def loop():
+	while True:
+		print get_adc(1)
+		time.sleep(1)
+
+if __name__ == "__main__":
+	loop()
